480dbfbf7113b349bf3d6d7450ff2ed2c7f511f6
youngky hermanto <yhermanto@gmail.com>
2017-09-28 07:05:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
 const app = "I don't do much."
 
 var kittens = ["Milo", " Otis", "Garfield"]
+
+function destructivelyAppendKitten(name) {
+  return kittens.push(name)
+}
+
+function destructivelyPrependKitten(name) {
+  return kittens.unshift(name)
+}
+
+function destructivelyRemoveLastKitten() {
+  return kittens.pop()
+}
+
+function destructivelyRemoveFirstKitten() {
+  return kittens.shift()
+}
